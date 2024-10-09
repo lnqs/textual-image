a5a4efb80253e589071c26ea8339eed8283be88f
Simon Hayessen <simon@lnqs.io>
2024-10-09 18:34:23 +0200

    fix: reorder preferred rendering methods
    Sixel is queried for first now. While TGP performs better, it's
    implementation in terminals other than Kitty doesn't really fit our
    needs. And as Kitty doesn't support Sixel, this should be the best order
    for now.

--- textual_image/renderable/__init__.py
+++ textual_image/renderable/__init__.py
@@ -16,12 +16,16 @@ is_tty = sys.__stdout__ and sys.__stdout__.isatty()
 
 Image: Type[TGPImage | SixelImage | HalfcellImage | UnicodeImage]
 
-if is_tty and tgp.query_terminal_support():
-    logger.debug("Terminal Graphics Protocol support detected")
-    Image = TGPImage
-elif is_tty and sixel.query_terminal_support():
+# TGP should be on top, as it performs way better than Sixel.
+# However, the only terminal with TGP unicode diacritic support I know of is Kitty.
+# Konsole and wezterm report TGP support, but don't work with our placeholder implementation, but do with Sixel.
+# As Kitty does *not* support Sixel, this order should be best in terms of compatibility.
+if is_tty and sixel.query_terminal_support():
     logger.debug("Sixel support detected")
     Image = SixelImage
+elif is_tty and tgp.query_terminal_support():
+    logger.debug("Terminal Graphics Protocol support detected")
+    Image = TGPImage
 elif is_tty:
     logger.debug("Connected to a terminal, using half cell rendering")
     Image = HalfcellImage
