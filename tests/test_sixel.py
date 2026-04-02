371a79a1cb203cdba75f73ee20e4a73cab5479ce
Simon Hayessen <simon@lnqs.io>
2026-04-02 22:08:51 +0200

    chore: support for Python 3.14
    Add support (i.e. testing) for/with Python 3.14.
    Based on the original idea of support the last three major Python
    versions, support for Python 3.10 and 3.11 is dropped.

--- tests/test_sixel.py
+++ tests/test_sixel.py
@@ -7,6 +7,6 @@ from textual_image._sixel import image_to_sixels
 
 def test_image_to_sixels(snapshot: SnapshotAssertion) -> None:
     with PILImage.open(TEST_IMAGE) as image:
-        image = image.resize((16, 16))
+        scaled_image = image.resize((16, 16))
 
-    assert image_to_sixels(image) == snapshot
+    assert image_to_sixels(scaled_image) == snapshot
