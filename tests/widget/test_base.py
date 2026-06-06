49da5cef9b1a640d25b51b9519ebe99f3001f54f
fires <158167689+fi-res@users.noreply.github.com>
2026-06-06 12:20:13 -0500

    feat(widget): add on_error callback support (#129)
    resolves #122

--- tests/widget/test_base.py
+++ tests/widget/test_base.py
@@ -69,3 +69,45 @@ def test_render_without_image() -> None:
     from textual_image.widget import Image
 
     assert Image().render() == ""
+
+
+@skipUnless(TEXTUAL_ENABLED, "Textual support disabled")
+async def test_on_error_callback() -> None:
+    from textual.app import App, ComposeResult
+    from textual.widgets import Static
+
+    from textual_image.widget import Image
+
+    class TestApp(App[None]):
+        def compose(self) -> ComposeResult:
+            yield Image(
+                TEST_IMAGE.read_bytes(), on_error=lambda e: Static(str(e))
+            )  # in this case we should wrap it with BytesIO
+
+    app = TestApp()
+
+    async with app.run_test():
+        widget = app.query_one(Image).children[0]
+        assert isinstance(widget, Static)
+        assert widget.content == "embedded null byte"
+
+
+@skipUnless(TEXTUAL_ENABLED, "Textual support disabled")
+async def test_raise_without_on_error() -> None:
+    from textual.app import App, ComposeResult
+
+    from textual_image.widget import Image
+
+    class TestApp(App[None]):
+        def compose(self) -> ComposeResult:
+            yield Image()
+
+    app = TestApp()
+
+    try:
+        async with app.run_test():
+            app.query_one(Image).image = TEST_IMAGE.read_bytes()
+    except Exception:
+        pass
+    else:
+        assert False
